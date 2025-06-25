66bd28c83ced410ed6e347033f02245753808141
Alfie <or22503@bristol.ac.uk>
2025-06-25 14:09:12 +0100

    correct virtual environment path

--- launch_labelme.sh
+++ launch_labelme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/or22503/labelme/labelme-env/bin/activate
+source /home/or22503/labelme-env/bin/activate
 cd /home/or22503/labelme
 exec labelme
 # This script activates the labelme virtual environment and launches the labelme application.
