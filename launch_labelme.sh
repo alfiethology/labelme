a13f0ad2b3224672947aaf2be5aa74af86dbb6a7
Alfie <or22503@bristol.ac.uk>
2025-06-25 13:59:10 +0100

    add launch_labelme bash script

---
+++ launch_labelme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /home/or22503/labelme/labelme-env/bin/activate
+cd /home/or22503/labelme
+exec labelme
+# This script activates the labelme virtual environment and launches the labelme application.
